18c7c87370159a48b41c8a5b2be2e59b14481189
Kirill <minchuk98@list.ru>
2023-02-25 16:32:03 +0300

    _5_Решение_Задачи

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿void Metod()
+{
+    int n = 3;
+    int j = 0; 
+
+    string[] array = { "qw", "21", "2dd2", "qwe122", ":-)", "- -", "hello" };
+
+    string[] NewArray = new string[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= n)
+        {
+            NewArray[j] = array[i];
+            Console.WriteLine($"{NewArray[j]}");
+            j++;  
+        }
+    }
+}
+Metod();
